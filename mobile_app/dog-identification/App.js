621e72ae382f9fdccac1884e09c3a40727b1b03f
hannah-schurman <93492390+hannah-schurman@users.noreply.github.com>
2022-03-01 09:30:35 -0800

    add files for mvp

--- mobile_app/dog-identification/App.js
+++ mobile_app/dog-identification/App.js
@@ -1,76 +1,173 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Image, ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as jpeg from 'jpeg-js'
+import ReactNative, { Image, ImageBackground, StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import {fetch, bundleResourceIO } from '@tensorflow/tfjs-react-native';
 import dog from './assets/dog_background.jpg';
-// import UploadImage from './UploadImage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import 'react-native-gesture-handler';
 import * as ImagePicker from 'expo-image-picker';
 
+ReactNative.LogBox.ignoreAllLogs(true);
+
 const Stack = createNativeStackNavigator();
 
-function Identification() {
-  let [selectedImage, setSelectedImage] = React.useState(null);
+export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+  const [breedDetector, setBreedDetector] = useState("")
 
-  let openImagePickerAsync = async () => {
-      let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  useEffect(() => {
+    async function loadModel() {
+      console.log("[+] Application Started")
+      const tfready = await tf.ready();
 
-      if (permissionResult.granted === false) {
-          alert('Permission to access camera roll is required!');
-          return;
-      }
+      console.log("[+] Loading custom breed detection model")
+      const modelJson = await require('./assets/model.json');
+      const modelWeight = await require('./assets/group1-shard1of1.bin');
+      const breedDetector = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeight));
 
-      let pickerResult = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images
-      });
-      if (pickerResult.cancelled === true) {
-          return;
-      }
+      console.log("[+] Loaded Model")
+      setBreedDetector(breedDetector)
+    }
+    loadModel()
+  }, []);
 
-      setSelectedImage({ localUri: pickerResult.uri });
+  function imageToTensor(rawImageData) {
+    console.log("Converting Image to tensor");
+    const TO_UINT8ARRAY = true;
+    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY);
+    console.log(`width of the image -> ${width} and ${height}`);
+
+    const buffer = new Uint8Array(width * height * 3);
+    let offset = 0;
+    for (let i = 0; i < buffer.length; i += 3) {
+        buffer[i] = data[offset];
+        buffer[i + 1] = data[offset + 1];
+        buffer[i + 2] = data[offset + 2];
+
+        offset += 4;
+    }
+
+    const normed = [];
+    for (let i = 0; i < buffer.length; i++) normed[i] = buffer[i] / 255.0; // Normalize
+
+    return tf.tensor3d(normed, [height, width, 3]).expandDims();
   };
 
-  if (selectedImage !== null) {
-      return (
-      <View style={styles.container}>
-          <Image source={{ uri: selectedImage.localUri }} />
-      </View>
-      );
-  }
+  function Identification() {
 
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-          onPress={openImagePickerAsync}
-          style={styles.button}>
-          <Text style={{ fontSize: 30, color: 'white' }}>Choose a photo</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+    const openImagePickerAsync = async () => {
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-function HomeScreen( {navigation}) {
-  
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={dog} resizeMode="cover" style={styles.image}>
-        <Text style={styles.header}>Paw Print</Text>
+        if (permissionResult.granted === false) {
+            alert('Permission to access camera roll is required!');
+            return;
+        }
+
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images
+        });
+        if (result.cancelled === true) {
+            return;
+        }
+
+        setSelectedImage({ localUri: result.uri });
+      };
+
+    const openCamera = async () => {
+        const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+        alert("You've refused to allow this appp to access your camera!");
+        return;
+        }
+
+        const result = await ImagePicker.launchCameraAsync();
+        result = imageToTensor(result).resizeBilinear([224,224]).reshape([1,224,224,3])
+
+        console.log(result);
+
+        if (result.cancelled === true) {
+        return ;
+        }
+
+        setSelectedImage({ localUri: result.uri });
+    }
+    if (selectedImage !== null) {
+        return (
+        <View style={{ marginTop: 20, flex: 1 }}>
+            <View style={{ marginBottom: 100, flex: 1 }}>
+            <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
+            </View>
+
+            <ScrollView style={styles.scrollView}>
+            <Text style={styles.text}> Dog breed here</Text>
+            <Text style={styles.text}> Dog info here</Text>
+            </ScrollView>
+        </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+
+        <View style={{flex: 0, marginBottom: 30, marginHorizontal: 50, }}> 
+        <Text style={styles.text1}>Welcome to Paw Print. Select an image of your furry friend and we will predict their breed!</Text>
+        </View>
 
         <TouchableOpacity
-          onPress={() => navigation.navigate('Identify')}
-          style={styles.button}>
-          <Text style={{ fontSize: 30, color: 'white' }}>Identify</Text>
+            onPress={openImagePickerAsync}
+            style={styles.button}>
+            <Text style={{ fontSize: 30, color: 'white' }}>Choose a photo</Text>
         </TouchableOpacity>
 
-        <StatusBar style="auto" />
+        <TouchableOpacity
+            onPress={openCamera}
+            style={styles.button}>
+            <Text style={{ fontSize: 30, color: 'white' }}>Take a photo</Text>
+        </TouchableOpacity>
 
-      </ImageBackground>
-    </View>
-  )
-}
+        </View>
+    )
+  }
 
-function App() {
+  const predictBreed = async() => {
+    try{
+      console.log("[+] Retrieving image from input")
+      const imageTensor = imageToTensor(selectedImage).resizeBilinear([224,224])
+
+      let result = await breedDetector.predict(imageTensor).data()
+
+      // find argmax of result and use as predicted breed
+
+      console.log("[+] Prediction Completed")
+    }catch{
+      console.log("[-] Unable to load image")
+    }
+    
+  }
+
+  function HomeScreen( {navigation}) {
+  
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={dog} resizeMode="cover" style={styles.image}>
+          <Text style={styles.header}>Paw Print</Text>
+
+          <TouchableOpacity
+          onPress={() => navigation.navigate('Identify')}
+          style={styles.button1}>
+          <Text style={{ fontSize: 30, color: 'black' }}>Click to begin</Text>
+          </TouchableOpacity>
+
+          <StatusBar style="auto" />
+
+        </ImageBackground>
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -107,7 +204,7 @@ const styles = StyleSheet.create({
     color: "black",
     marginTop: 10,
     marginBottom: 500,
-    ///marginHorizontal: 30,
+    marginHorizontal: 30,
   },
   button: {
     backgroundColor: 'orange', 
@@ -115,7 +212,34 @@ const styles = StyleSheet.create({
     marginHorizontal: 50,
     opacity: 0.9,
     borderRadius: 10,
-    padding: 10
-  }
+    padding: 10,
+    marginTop: 10,
+  },
+  button1: {
+    backgroundColor: 'white', 
+    alignItems: 'center',
+    marginHorizontal: 50,
+    opacity: 0.5,
+    borderRadius: 10,
+    padding: 10,
+  },
+  thumbnail: {
+    width: 375,
+    height: 300,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 50,
+  },
+  text1: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  scrollView: {
+    backgroundColor: 'grey',
+  },
+
 });
-export default App;
